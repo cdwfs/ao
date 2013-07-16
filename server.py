30ecebf2a741e12cc67f96aab02562fe63acb41e
Cort <github@dangerware.org>
2013-07-16 12:41:54 -0700

    server.py now uses port 80; prints hosted directory and URL at startup.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
+import os
 import sys
 
-port = 8000
+port = 80
 
 if sys.version_info.major == 3:
     import http.server
+    import socketserver
     handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("",port), handler)
 elif sys.version_info.major == 2:
@@ -11,6 +13,9 @@ elif sys.version_info.major == 2:
     import SocketServer
     handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     httpd = SocketServer.TCPServer(("",port), handler)
-
-print("Serving at port %d\n" % port)
+if port == 80:
+    server_string = "localhost"
+else:
+    server_string = "localhost:%d" % port
+print("Serving %s at http://%s\n" % (os.getcwd(),server_string))
 httpd.serve_forever()
