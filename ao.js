2b000b95bedc10cc4f5e1a5a46def218a73521be
Cort <github@dangerware.org>
2013-07-23 23:28:12 -0700

    Fixed jslint warnings

--- ao.js
+++ ao.js
@@ -1,20 +1,28 @@
-"use strict";
-
-var createAoGrid = function(sizeX, sizeY, sizeZ) {
-  var createArray3d = function(sizeX,sizeY, sizeZ, defaultVal) {
-    var arr = [];
-    for(var z=0; z<sizeZ; ++z) {
-      arr[z] = [];
-      for(var y=0; y<sizeY; ++y) {
-        arr[z][y] = [];
-        for(var x=0; x<sizeX; ++x) {
-          arr[z][y][x] = defaultVal;
-        }
-      }
-    }
-    return arr;
-  };
-  var Octant = {
+/*jslint indent: 2, white: true, bitwise: true, continue: true */
+function createAoGrid(sizeX, sizeY, sizeZ) {
+  "use strict";
+  var m_cells, m_verts, m_isDirty = false, m_octant, m_cellOffsets, m_aoFactors;
+  // Offsets from a given vertex (at [0,0,0]) to cells which should be considered for
+  // visibility testing in a particular octant. Each cell's visibility corresponds to
+  // a bit in the bitmask used to index into m_aoFactors[].
+  m_cellOffsets = [
+    {x:0,y:0,z:1},
+    {x:0,y:1,z:0},
+    {x:1,y:0,z:0}
+  ];
+  // Lookup table mapping cell visibility bitmasks to AO factors.
+  m_aoFactors = [ 
+    1.000, // 000
+    0.666, // 001
+    0.666, // 010
+    0.333, // 011
+    0.666, // 100
+    0.333, // 101
+    0.333, // 110
+    0.000  // 111
+  ];
+  // Fake enum
+  m_octant = {
     nXnYnZ: 0,
     pXnYnZ: 1,
     nXpYnZ: 2,
@@ -22,13 +30,26 @@ var createAoGrid = function(sizeX, sizeY, sizeZ) {
     nXnYpZ: 4,
     pXnYpZ: 5,
     nXpYpZ: 6,
-    pXpYpZ: 7,
+    pXpYpZ: 7
   };
 
-  var m_cells = createArray3d(sizeX+0, sizeY+0, sizeZ+0, false); // is each cell filled? (true/false)
-  var m_verts = createArray3d(sizeX+1, sizeY+1, sizeZ+1, null);
-  var m_isDirty = false;
-  var m_setCell = function(cx,cy,cz,val) {
+  function createArray3d(sizeX, sizeY, sizeZ, defaultVal) {
+    var x, y, z, arr = [];
+    for (z=0; z<sizeZ; z+=1) {
+      arr[z] = [];
+      for (y=0; y<sizeY; y+=1) {
+        arr[z][y] = [];
+        for (x=0; x<sizeX; x+=1) {
+          arr[z][y][x] = defaultVal;
+        }
+      }
+    }
+    return arr;
+  }
+
+  m_cells = createArray3d(sizeX,   sizeY,   sizeZ,   false); // is each cell filled? (true/false)
+  m_verts = createArray3d(sizeX+1, sizeY+1, sizeZ+1, null);
+  function m_setCell(cx,cy,cz,val) {
     if (cx < 0 || cx >= sizeX || cy < 0 || cy >= sizeY || cz < 0 || cz >= sizeZ) {
       throw {
         name: "BoundsError",
@@ -38,24 +59,14 @@ var createAoGrid = function(sizeX, sizeY, sizeZ) {
     m_cells[cz][cy][cx] = val;
     m_isDirty = true;
     return this;
-  };
-  var m_getCell = function(cx,cy,cz) {
+  }
+  function m_getCell(cx,cy,cz) {
     if (cx < 0 || cx >= sizeX || cy < 0 || cy >= sizeY || cz < 0 || cz >= sizeZ) {
       return null; // out-of-bounds access are legal; there's nothing there.
     }
     return m_cells[cz][cy][cx];
-  };
-  var m_setAoFactor = function(vx,vy,vz,dir,aoFactor) {
-    if (vx < 0 || vx > sizeX || vy < 0 || vy > sizeY || vz < 0 || vz > sizeZ) {
-      throw {
-        name: "BoundsError",
-        message: "setCell: voxel cell index ["+vx+","+vy+","+vz+"] is out of bounds."
-      };
-    }
-    m_verts[vz][vy][vx][dir] = aoFactor;
-    return this;
-  };
-  var m_getAoFactor = function(vx,vy,vz,dir) {
+  }
+  function m_getAoFactor(vx, vy, vz, dir) {
     if (vx < 0 || vx > sizeX || vy < 0 || vy > sizeY || vz < 0 || vz > sizeZ) {
       return null; // out-of-bounds access are legal; there's nothing there.
     }
@@ -66,97 +77,97 @@ var createAoGrid = function(sizeX, sizeY, sizeZ) {
       };
     }
     return m_verts[vz][vy][vx][dir];
-  };
+  }
 
-  var m_clear = function() {
-    for(var cz=0; cz<sizeZ; cz += 1) {
-      for(var cy=0; cy<sizeY; cy += 1) {
-        for(var cx=0; cx<sizeX; cx += 1) {
-          m_setCell(cx,cy,cz,false);
+  function m_clear() {
+	var cx, cy, cz, vx, vy, vz;
+    for(cz=0; cz<sizeZ; cz += 1) {
+      for(cy=0; cy<sizeY; cy += 1) {
+        for(cx=0; cx<sizeX; cx += 1) {
+          m_setCell(cx, cy, cz, false);
         }
       }
     }
-    for(var vz=0; vz<=sizeZ; vz += 1) {
-      for(var vy=0; vy<=sizeY; vy += 1) {
-        for(var vx=0; vx<=sizeX; vx += 1) {
+    for(vz=0; vz<=sizeZ; vz += 1) {
+      for(vy=0; vy<=sizeY; vy += 1) {
+        for(vx=0; vx<=sizeX; vx += 1) {
           m_verts[vz][vy][vx] = null;
         }
       }
     }
     m_isDirty = false;
-  };
+  }
 
-  // Offsets from a given vertex (at [0,0,0]) to cells which should be considered for
-  // visibility testing in a particular octant. Each cell's visibility corresponds to
-  // a bit in the bitmask used to index into m_aoFactors[].
-  var m_cellOffsets = [
-    {x:0,y:0,z:1},
-    {x:0,y:1,z:0},
-    {x:1,y:0,z:0},
-  ];
-  var m_aoFactors = [ // Lookup table mapping cell visibility bitmasks to AO factors.
-    1.000, // 000
-    0.666, // 001
-    0.666, // 010
-    0.333, // 011
-    0.666, // 100
-    0.333, // 101
-    0.333, // 110
-    0.000, // 111
-  ];
-  var flipToOctant = function(offset,octant) {
+  function flipToOctant(offset,octant) {
     return {
       x: (octant & 0x1) ? offset.x : (-1-offset.x),
       y: (octant & 0x2) ? offset.y : (-1-offset.y),
-      z: (octant & 0x4) ? offset.z : (-1-offset.z),
+      z: (octant & 0x4) ? offset.z : (-1-offset.z)
     };
-  };
-  var m_getAoFactorForOctant = function(gridVert, octant) {
+  }
+  function m_getAoFactorForOctant(gridVert, octant) {
     // ASSUMPTION: the octant is not fully occluded -- cell at flipped offset [0,0,0]
     // is 0/null,
-    var bitMask = 0x00000000;
-    for(var iCell=0; iCell<m_cellOffsets.length; iCell += 1) {
-      var offset = flipToOctant(m_cellOffsets[iCell],octant);
+    var bitMask = 0x00000000, iCell, offset;
+    for(iCell=0; iCell<m_cellOffsets.length; iCell += 1) {
+      offset = flipToOctant(m_cellOffsets[iCell],octant);
       if ( m_getCell(gridVert.x+offset.x, gridVert.y+offset.y, gridVert.z+offset.z) ) {
         bitMask |= (1<<iCell);
       }
     }
     return m_aoFactors[bitMask];
-  };
-  var m_updateGridAo = function() {
+  }
+  function m_updateGridAo() {
+	var vx, vy, vz, gridVert, isCellFilled, anyCellFilled, results, iOct, of;
     if (!m_isDirty) {
       return this; // No updates necessary
     }
     // Iterate over vertices
-    for(var vz=0; vz<=sizeZ; vz += 1) {
-      for(var vy=0; vy<=sizeY; vy += 1) {
-        for(var vx=0; vx<=sizeX; vx += 1) {
-          var gridVert = {x:vx,y:vy,z:vz};
+    for(vz=0; vz<=sizeZ; vz += 1) {
+      for(vy=0; vy<=sizeY; vy += 1) {
+        for(vx=0; vx<=sizeX; vx += 1) {
+          gridVert = {x:vx,y:vy,z:vz};
           // Query cells at offset [0,0,0] in all octants.
           // Keep track of which are occluded; these are the cells we'll need to update.
           // at the end of the loop.
-          var isCellFilled = new Array(8);
-          var anyCellFilled = false;
-          anyCellFilled = (isCellFilled[Octant.nXnYnZ] = m_getCell(vx-1,vy-1,vz-1)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.pXnYnZ] = m_getCell(vx-0,vy-1,vz-1)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.nXpYnZ] = m_getCell(vx-1,vy-0,vz-1)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.pXpYnZ] = m_getCell(vx-0,vy-0,vz-1)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.nXnYpZ] = m_getCell(vx-1,vy-1,vz-0)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.pXnYpZ] = m_getCell(vx-0,vy-1,vz-0)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.nXpYpZ] = m_getCell(vx-1,vy-0,vz-0)) || anyCellFilled;
-          anyCellFilled = (isCellFilled[Octant.pXpYpZ] = m_getCell(vx-0,vy-0,vz-0)) || anyCellFilled;
-          var results = {pX:0,nX:0, pY:0,nY:0, pZ:0,nZ:0};
+          isCellFilled = [];
+		  isCellFilled.length = 8;
+          isCellFilled[m_octant.nXnYnZ] = m_getCell(vx-1,vy-1,vz-1);
+          isCellFilled[m_octant.pXnYnZ] = m_getCell(vx,  vy-1,vz-1);
+          isCellFilled[m_octant.nXpYnZ] = m_getCell(vx-1,vy,  vz-1);
+          isCellFilled[m_octant.pXpYnZ] = m_getCell(vx,  vy,  vz-1);
+          isCellFilled[m_octant.nXnYpZ] = m_getCell(vx-1,vy-1,vz  );
+          isCellFilled[m_octant.pXnYpZ] = m_getCell(vx,  vy-1,vz  );
+          isCellFilled[m_octant.nXpYpZ] = m_getCell(vx-1,vy,  vz  );
+          isCellFilled[m_octant.pXpYpZ] = m_getCell(vx,  vy,  vz  );
+          anyCellFilled = false;
+		  for(iOct=0; iOct<8; iOct+=1) {
+			anyCellFilled = anyCellFilled || isCellFilled[iOct];
+		  }
           if (!anyCellFilled) {
             m_verts[vz][vy][vx] = null; // no neighboring cells are filled, so AO is undefined.
             continue;
           }
           // Compute full AO visibility test for all unoccluded octants.
           // Cache results for each of the six axis-aligned half-spaces: +x, -x, etc.
-          for(var iOct=0; iOct<8; iOct += 1) {
-            var of = 0.25 * (isCellFilled[iOct] ? 0.0 : m_getAoFactorForOctant(gridVert, iOct));
-            (iOct & 0x1) ? (results.pX += of) : (results.nX += of);
-            (iOct & 0x2) ? (results.pY += of) : (results.nY += of);
-            (iOct & 0x4) ? (results.pZ += of) : (results.nZ += of);
+          results = {pX:0,nX:0, pY:0,nY:0, pZ:0,nZ:0};
+          for(iOct=0; iOct<8; iOct += 1) {
+            of = 0.25 * (isCellFilled[iOct] ? 0.0 : m_getAoFactorForOctant(gridVert, iOct));
+            if (iOct & 0x1) {
+			  results.pX += of;
+			} else {
+			  results.nX += of;
+			}
+            if (iOct & 0x2) {
+			  results.pY += of;
+			} else {
+			  results.nY += of;
+			}
+            if (iOct & 0x4) {
+			  results.pZ += of;
+			} else {
+			  results.nZ += of;
+			}
           }
           m_verts[vz][vy][vx] = results;
         }
@@ -164,7 +175,7 @@ var createAoGrid = function(sizeX, sizeY, sizeZ) {
     }
     m_isDirty = false;
     return this;
-  };
+  }
 
   return {
     sizeX: function() { return sizeX; },
@@ -176,6 +187,6 @@ var createAoGrid = function(sizeX, sizeY, sizeZ) {
     getCell: m_getCell,
     //setAoFactor: m_setAoFactor,
     getAoFactor: m_getAoFactor,
-    update: m_updateGridAo,
+    update: m_updateGridAo
   };
-};
+}
--- program.js
+++ program.js
@@ -1,28 +1,34 @@
-"use strict";
-
+/*jslint indent:2, white:true, browser:true, bitwise:true */
+/*global THREE, Stats, createAoGrid */
 var aoLookupTex = THREE.ImageUtils.loadTexture( "cube_ao_lookup.png" );
 var cubeUniforms = {
-  texture:    { type: "t",  value: aoLookupTex },
+  texture:    { type: "t",  value: aoLookupTex }
 };
 var aoCubeMaterial = new THREE.ShaderMaterial( {
   uniforms:       cubeUniforms,
   vertexShader:   document.getElementById( 'vertexShader' ).textContent,
-  fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+  fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 } );
+var scene;
+var container;
+var stats;
+var camera, renderer;
+var cameraControls, effectController;
+var clock = new THREE.Clock();
+var rolloverCubeMesh;
+var voxelGrid;
+var cubes = [];
+var dirLight;
+var mouse = new THREE.Vector3(0,10000,0.5);
+var projector = new THREE.Projector();
+var isShiftDown = false;
+var isCtrlDown = false;
+var rolloverCubePos = new THREE.Vector3();
 
-var createAoCubeGeometry = function() {
-  var Octant = {
-    nXnYnZ: 0,
-    pXnYnZ: 1,
-    nXpYnZ: 2,
-    pXpYnZ: 3,
-    nXnYpZ: 4,
-    pXnYpZ: 5,
-    nXpYpZ: 6,
-    pXpYpZ: 7,
-  };
-
-  var cubeGeom = new THREE.Geometry();
+function createAoCubeGeometry() {
+  "use strict";
+  var cubeGeom, iFace;
+  cubeGeom = new THREE.Geometry();
   //    +y
   //    2-----3
   //   /|    /|
@@ -48,113 +54,118 @@ var createAoCubeGeometry = function() {
   cubeGeom.faces.push( new THREE.Face4(0,2,3,1, new THREE.Vector3( 0, 0,-1)) ); // -Z
   cubeGeom.faces.push( new THREE.Face4(4,5,7,6, new THREE.Vector3( 0, 0, 1)) ); // +Z
 
-  for(var iFace=0; iFace<6; iFace += 1) {
+  for(iFace=0; iFace<6; iFace += 1) {
     cubeGeom.faceVertexUvs[0].push( [new THREE.Vector2(0,0), new THREE.Vector2(1,0), new THREE.Vector2(1,1), new THREE.Vector2(0,1)] );
   }
 
   return cubeGeom;
-};
+}
 
-var createVoxelGrid = function(sizeX,sizeY,sizeZ) {
-  var m_urCubeGeom = createAoCubeGeometry();
-  var createArray3d = function(sizeX,sizeY, sizeZ, defaultVal) {
-    var arr = [];
-    for(var z=0; z<sizeZ; ++z) {
+function createVoxelGrid(sizeX, sizeY, sizeZ) {
+  "use strict";
+  var m_urCubeGeom = createAoCubeGeometry(), m_grid, m_cubes, m_aoGrid;
+  function createArray3d(sizeX, sizeY, sizeZ, defaultVal) {
+    var arr = [], x, y, z;
+    for(z=0; z<sizeZ; z += 1) {
       arr[z] = [];
-      for(var y=0; y<sizeY; ++y) {
+      for(y=0; y<sizeY; y += 1) {
         arr[z][y] = [];
-        for(var x=0; x<sizeX; ++x) {
+        for(x=0; x<sizeX; x += 1) {
           arr[z][y][x] = defaultVal;
         }
       }
     }
     return arr;
-  };
+  }
 
-  var m_grid = createArray3d(sizeX,sizeY,sizeZ,-1);
-  var m_cubes = [];
-  var m_aoGrid = createAoGrid(sizeX,sizeY,sizeZ);
+  m_grid = createArray3d(sizeX,sizeY,sizeZ,-1);
+  m_cubes = [];
+  m_aoGrid = createAoGrid(sizeX, sizeY, sizeZ);
 
-  var m_addCube = function(cx,cy,cz) {
+  function m_addCube(cx, cy, cz) {
+    var newCube;
     if (cx < 0 || cx >= sizeX || cy < 0 || cy >= sizeY || cz < 0 || cz >= sizeZ) {
       return this; // out of bounds silently fails
     }
-    if (m_grid[cz][cy][cx] != -1) {
+    if (m_grid[cz][cy][cx] !== -1) {
       throw {
         name: "VoxelError",
         message: "Voxel cell ["+cx+","+cy+","+cz+"] is already filled!"
       };
     }
     m_aoGrid.setCell(cx,cy,cz,true);
-    var newCube = new THREE.Mesh(m_urCubeGeom.clone(), aoCubeMaterial);
+    newCube = new THREE.Mesh(m_urCubeGeom.clone(), aoCubeMaterial);
     newCube.position.set(cx+0.5,cy+0.5,cz+0.5);
     m_grid[cz][cy][cx] = m_cubes.length;
     m_cubes.push(newCube);
     scene.add(newCube);
     return this;
-  };
+  }
 
-  var m_removeCube = function(cx,cy,cz) {
+  function m_removeCube(cx, cy, cz) {
+    var cubeIndex, oldCube, lastCube;
     if (cx < 0 || cx >= sizeX || cy < 0 || cy >= sizeY || cz < 0 || cz >= sizeZ) {
       return this; // out of bounds silently fails
     }
-    if (m_grid[cz][cy][cx] == -1) {
+    if (m_grid[cz][cy][cx] === -1) {
       throw {
         name: "VoxelError",
         message: "Voxel cell ["+cx+","+cy+","+cz+"] is already empty!"
       };
     }
     m_aoGrid.setCell(cx,cy,cz,false);
-    var cubeIndex = m_grid[cz][cy][cx];
+    cubeIndex = m_grid[cz][cy][cx];
     m_grid[cz][cy][cx] = -1;
-    var oldCube = m_cubes[cubeIndex];
-    var lastCube = m_cubes.pop();
-    if (oldCube != lastCube) {
+    oldCube = m_cubes[cubeIndex];
+    lastCube = m_cubes.pop();
+    if (oldCube !== lastCube) {
       m_cubes[cubeIndex] = lastCube;
     }
     scene.remove(oldCube);
     return this;
-  };
+  }
 
-  var m_updateCubeAo = function(cx,cy,cz) {
-    if (m_grid[cz][cy][cx] == -1) {
+  function m_updateCubeAo(cx, cy, cz) {
+    var cubeGeom, tx = cx+0.5, ty = cy+0.5, tz = cz+0.5, faceNormals, faceAoFactors = [],
+	  iFace, face, normDir, faceIndices, iCorner, vert, lutX, lutY;
+    if (m_grid[cz][cy][cx] === -1) {
       throw {
         name: "VoxelError",
         message: "Voxel cell ["+cx+","+cy+","+cz+"] is already empty!"
       };
     }
-    var cubeGeom = m_cubes[ m_grid[cz][cy][cx] ].geometry;
-    var tx = cx+0.5, ty = cy+0.5, tz = cz+0.5;
-    var faceNormals = ["nX","pX", "nY", "pY", "nZ", "pZ"];
-    var faceAoFactors = new Array(4);
-    for(var iFace=0; iFace<6; ++iFace) {
+    cubeGeom = m_cubes[ m_grid[cz][cy][cx] ].geometry;
+    faceNormals = ["nX","pX", "nY", "pY", "nZ", "pZ"];
+    faceAoFactors.length = 4;
+    for(iFace=0; iFace<6; iFace+=1) {
       // Look up AO factor at all four corners
-      var face = cubeGeom.faces[iFace];
-      var normDir = faceNormals[iFace];
-      var faceIndices = [face.a, face.b, face.c, face.d];
-      for(var iCorner=0; iCorner<4; ++iCorner) {
-        var vert = cubeGeom.vertices[faceIndices[iCorner]];
+      face = cubeGeom.faces[iFace];
+      normDir = faceNormals[iFace];
+      faceIndices = [face.a, face.b, face.c, face.d];
+      for(iCorner=0; iCorner<4; iCorner+=1) {
+        vert = cubeGeom.vertices[faceIndices[iCorner]];
         faceAoFactors[iCorner] = m_aoGrid.getAoFactor(tx+vert.x, ty+vert.y, tz+vert.z, normDir) * 0xF;
       }
       //console.log("face "+iFace+" factors: "+faceAoFactors[0]+" "+faceAoFactors[1]+" "+faceAoFactors[2]+" "+faceAoFactors[3])
       // Combine AO factors into new UVs for this face
-      var lutX = (1/1024) + ((faceAoFactors[0] & 0xF) | ((faceAoFactors[1] & 0xF) << 4)) / 256;
-      var lutY = (1/1024) + ((faceAoFactors[3] & 0xF) | ((faceAoFactors[2] & 0xF) << 4)) / 256;
-      cubeGeom.faceVertexUvs[0][iFace][0].set( lutX+0,      1.0 - (lutY+0)       );
-      cubeGeom.faceVertexUvs[0][iFace][1].set( lutX+(1/512),1.0 - (lutY+0)       );
+      lutX = (1/1024) + ((faceAoFactors[0] & 0xF) | ((faceAoFactors[1] & 0xF) << 4)) / 256;
+      lutY = (1/1024) + ((faceAoFactors[3] & 0xF) | ((faceAoFactors[2] & 0xF) << 4)) / 256;
+      cubeGeom.faceVertexUvs[0][iFace][0].set( lutX,        1.0 -  lutY          );
+      cubeGeom.faceVertexUvs[0][iFace][1].set( lutX+(1/512),1.0 -  lutY          );
       cubeGeom.faceVertexUvs[0][iFace][2].set( lutX+(1/512),1.0 - (lutY+(1/512)) );
-      cubeGeom.faceVertexUvs[0][iFace][3].set( lutX+0,      1.0 - (lutY+(1/512)) );
+      cubeGeom.faceVertexUvs[0][iFace][3].set( lutX,        1.0 - (lutY+(1/512)) );
     }
     cubeGeom.uvsNeedUpdate = true;
-  };
+  }
 
-  var m_update = function() {
+  function m_update() {
+    var cx, cy, cz;
     if (m_aoGrid.isDirty()) {
       m_aoGrid.update();
-      for(var cz=0; cz<sizeZ; ++cz) {
-        for(var cy=0; cy<sizeY; ++cy) {
-          for(var cx=0; cx<sizeX; ++cx) {
-            if (m_grid[cz][cy][cx] != -1) {
+      for(cz=0; cz<sizeZ; cz+=1) {
+        for(cy=0; cy<sizeY; cy+=1) {
+          for(cx=0; cx<sizeX; cx+=1) {
+            if (m_grid[cz][cy][cx] !== -1) {
               m_updateCubeAo(cx,cy,cz);
             }
           }
@@ -170,13 +181,13 @@ var createVoxelGrid = function(sizeX,sizeY,sizeZ) {
     cubes: m_cubes,
     addCube: m_addCube,
     removeCube: m_removeCube,
-    update: m_update,
+    update: m_update
   };
-};
+}
 
-var rolloverCubePos = new THREE.Vector3();
-var normalMatrix = new THREE.Matrix3();
-var updateRolloverCube = function( intersector ) {
+function updateRolloverCube( intersector ) {
+  "use strict";
+  var normalMatrix = new THREE.Matrix3();
   normalMatrix.getNormalMatrix( intersector.object.matrixWorld );
 
   rolloverCubePos.copy( intersector.face.normal );
@@ -185,9 +196,9 @@ var updateRolloverCube = function( intersector ) {
 
   rolloverCubePos.add(intersector.point);
 
-  rolloverCubePos.x = Math.floor( rolloverCubePos.x / 1 ) * 1 + 0.5;
-  rolloverCubePos.y = Math.floor( rolloverCubePos.y / 1 ) * 1 + 0.5;
-  rolloverCubePos.z = Math.floor( rolloverCubePos.z / 1 ) * 1 + 0.5;
+  rolloverCubePos.x = Math.floor(rolloverCubePos.x) + 0.5;
+  rolloverCubePos.y = Math.floor(rolloverCubePos.y) + 0.5;
+  rolloverCubePos.z = Math.floor(rolloverCubePos.z) + 0.5;
   if (rolloverCubePos.x < 0 || rolloverCubePos.x > voxelGrid.sizeX() ||
       rolloverCubePos.y < 0 || rolloverCubePos.y > voxelGrid.sizeY() ||
       rolloverCubePos.z < 0 || rolloverCubePos.z > voxelGrid.sizeZ()) {
@@ -198,27 +209,14 @@ var updateRolloverCube = function( intersector ) {
   rolloverCubeMesh.material.visible = true;
 }
 
-
-var container;
-var stats;
-var scene, camera, renderer;
-var cameraControls, effectController;
-var clock = new THREE.Clock();
-var rolloverCubeMesh;
-var voxelGrid;
-var cubes = [];
-var dirLight;
-var mouse = new THREE.Vector3(0,10000,0.5);
-var projector = new THREE.Projector();
-var isShiftDown = false;
-var isCtrlDown = false;
-
-var fillScene = function() {
+function fillScene() {
+  "use strict";
+  var rolloverGeom, rolloverMat, axes;
   scene = new THREE.Scene();
   //scene.fog = new THREE.Fog( 0x808080, 5, 5.5 );
 
-  var rolloverGeom = new THREE.CubeGeometry( 1, 1, 1 );
-  var rolloverMat  = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
+  rolloverGeom = new THREE.CubeGeometry( 1, 1, 1 );
+  rolloverMat  = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
   rolloverCubeMesh = new THREE.Mesh( rolloverGeom, rolloverMat );
   scene.add( rolloverCubeMesh );
 
@@ -234,59 +232,67 @@ var fillScene = function() {
   dirLight = new THREE.DirectionalLight(0xffffff, 1.0);
   scene.add(dirLight);
 
-  var axes = new THREE.AxisHelper( 100 );
+  axes = new THREE.AxisHelper( 100 );
 	axes.material.depthTest = true;
 	axes.material.transparent = true;
 	axes.matrixAutoUpdate = false;
 	axes.visible = true;
   scene.add(axes);
-};
+}
 
-var onWindowResize = function( event ) {
+function onWindowResize( /*event*/ ) {
+  "use strict";
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
-};
-var onDocumentMouseMove = function( event ) {
+}
+function onDocumentMouseMove( event ) {
+  "use strict";
   event.preventDefault();
   mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
   mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-};
-var onDocumentKeyDown = function( event ) {
+}
+function onDocumentKeyDown( event ) {
+  "use strict";
   switch( event.keyCode ) {
     case 16: isShiftDown = true; break;
     case 17: isCtrlDown = true; break;
   }
-};
-
-var onDocumentKeyUp = function( event ) {
+}
+function onDocumentKeyUp( event ) {
+  "use strict";
   switch ( event.keyCode ) {
     case 16: isShiftDown = false; break;
     case 17: isCtrlDown = false; break;
   }
-};
-var onDocumentMouseDown = function( event ) {
-  var raycaster = projector.pickingRay( mouse.clone(), camera );
-  var intersects = raycaster.intersectObjects( voxelGrid.cubes );
+}
+function onDocumentMouseDown( /*event*/ ) {
+  "use strict";
+  var raycaster, intersects, intersector, pos;
+  raycaster = projector.pickingRay( mouse.clone(), camera );
+  intersects = raycaster.intersectObjects( voxelGrid.cubes );
   if ( intersects.length > 0 ) {
-    var intersector = intersects[0];
+    intersector = intersects[0];
     if ( isCtrlDown ) {
       // delete cube
-      var pos = intersector.object.position;
+      pos = intersector.object.position;
       voxelGrid.removeCube(Math.floor(pos.x), Math.floor(pos.y), Math.floor(pos.z));
     } else if (isShiftDown) {
       // create cube
       updateRolloverCube( intersector );
-      var pos = rolloverCubePos;
+      pos = rolloverCubePos;
       voxelGrid.addCube(Math.floor(pos.x), Math.floor(pos.y), Math.floor(pos.z));
     }
       voxelGrid.update(); // Update AO to reflect changes.
   }
-};
-var init = function() {
-	var canvasWidth = 800;//window.innerWidth;
-	var canvasHeight = 600;//window.innerHeight;
-	var canvasRatio = canvasWidth / canvasHeight;
+}
+
+function init() {
+  "use strict";
+  var canvasWidth, canvasHeight, canvasRatio;
+  canvasWidth = window.innerWidth;
+  canvasHeight = window.innerHeight;
+  canvasRatio = canvasWidth / canvasHeight;
   renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.gammaInput = true;
 	renderer.gammaOutput = true;
@@ -302,15 +308,15 @@ var init = function() {
   stats.domElement.style.top = '00px';
   container.appendChild( stats.domElement );
 
-	// CAMERA
-	camera = new THREE.PerspectiveCamera( 45, canvasRatio, 0.1, 1000.0 );
-	camera.position.set( 20,20,20 );
+  // CAMERA
+  camera = new THREE.PerspectiveCamera( 45, canvasRatio, 0.1, 1000.0 );
+  camera.position.set( 20,20,20 );
   camera.lookAt( new THREE.Vector3(0,0,0) );
 
   cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
   cameraControls.target.set(0,0,0);
 
-	fillScene();
+  fillScene();
 
   //var gui = new dat.GUI();
   //gui.add(this, 'factor0', 0, 15).onChange(applyAndUpdate);
@@ -325,20 +331,21 @@ var init = function() {
   document.addEventListener( 'mousedown', onDocumentMouseDown, false );
   document.addEventListener( 'keydown', onDocumentKeyDown, false );
   document.addEventListener( 'keyup', onDocumentKeyUp, false );
-};
+}
 
-var render = function() {
-	var delta = clock.getDelta();
+function render() {
+  "use strict";
+  var delta = clock.getDelta(), raycaster, intersects, intersector;
 
   THREE.AxisHelper(100);
 
   cameraControls.update(delta);
 
   // find intersections
-  var raycaster = projector.pickingRay( mouse.clone(), camera );
-  var intersects = raycaster.intersectObjects( voxelGrid.cubes );
+  raycaster = projector.pickingRay( mouse.clone(), camera );
+  intersects = raycaster.intersectObjects( voxelGrid.cubes );
   if ( intersects.length > 0 ) {
-    var intersector = intersects[0];
+    intersector = intersects[0];
     if ( intersector ) {
       updateRolloverCube( intersector );
       rolloverCubeMesh.position = rolloverCubePos;
@@ -347,16 +354,16 @@ var render = function() {
   else {
     rolloverCubeMesh.material.visible = false;
   }
-
   renderer.render(scene,camera);
-};
+}
 
-var animate = function() {
+function animate() {
+  "use strict";
   window.requestAnimationFrame(animate);
-  dirLight.position.copy(cameraControls.object.position)
+  dirLight.position.copy(cameraControls.object.position);
   render();
-  stats.update()
-};
+  stats.update();
+}
 
 init();
 animate();
